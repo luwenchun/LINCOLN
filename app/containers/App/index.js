773ccda8ead9a16629978c4c518d63b22e33178a
Meng <xiongmeng@youyou.com>
2018-07-03 17:54:00 +0800

    ..

--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -5,22 +5,22 @@ import styled from 'styled-components';
 import { Switch, Route } from 'react-router-dom';
 // import FeaturePage from 'containers/FeaturePage/Loadable';
 import NotFoundPage from 'containers/NotFoundPage/Loadable';
-import DriveList from 'containers/drive/drive.list';
-import DriveDetail from 'containers/drive/drive.detail';
-import DriveAdd from 'containers/drive/drive.add';
-import Complete from 'containers/drive/complete';
+// import DriveList from 'containers/drive/drive.list';
+// import DriveDetail from 'containers/drive/drive.detail';
+// import DriveAdd from 'containers/drive/drive.add';
+// import Complete from 'containers/drive/complete';
 
-import CDriveAdd from 'containers/cdrive/drive.add';
-import CDriveMap from 'containers/cdrive/drive.map';
+// import CDriveAdd from 'containers/cdrive/drive.add';
+// import CDriveMap from 'containers/cdrive/drive.map';
 // import CDriveSearch from 'containers/cdrive/drive.search';
-import CDriveList from 'containers/cdrive/drive.list';
-import CDriveDetail from 'containers/cdrive/drive.detail';
-import CDriveFeedback from 'containers/cdrive/drive.feedback';
-import CDriveRegister from 'containers/cdrive/drive.register';
-import CDriveFeedbackList from 'containers/cdrive/drive.feedback.list';
+// import CDriveList from 'containers/cdrive/drive.list';
+// import CDriveDetail from 'containers/cdrive/drive.detail';
+// import CDriveFeedback from 'containers/cdrive/drive.feedback';
+// import CDriveRegister from 'containers/cdrive/drive.register';
+// import CDriveFeedbackList from 'containers/cdrive/drive.feedback.list';
 
 import Chat from 'containers/chat/Loadable';
-import News from 'containers/news/Loadable';
+// import News from 'containers/news/Loadable';
 import MaterialList from 'containers/materialList/Loadable';
 
 const AppWrapper = styled.div`
@@ -48,24 +48,24 @@ export default function App() {
       <Switch>
         
         {/* 微信公众平台 */}
-        <Route path="/wchat/drive" component={DriveList} />
+        {/* <Route path="/wchat/drive" component={DriveList} />
         <Route path="/wchat/driveDetail" component={DriveDetail} />
         <Route path="/wchat/driveAdd" component={DriveAdd} />
-        <Route path="/wchat/complete" component={Complete} />
+        <Route path="/wchat/complete" component={Complete} /> */}
 
         {/* 微信C端 */}
-        <Route path="/wchat/cdriveAdd" component={CDriveAdd} />
+        {/* <Route path="/wchat/cdriveAdd" component={CDriveAdd} />
         <Route path="/wchat/cdriveMap" component={CDriveMap} />
         <Route path="/wchat/cdriveList" component={CDriveList} />
         <Route path="/wchat/cdriveDetail" component={CDriveDetail} />
         <Route path="/wchat/cdriveFeeback" component={CDriveFeedback} />
         <Route path="/wchat/cdriveRegister" component={CDriveRegister} />
-        <Route path="/wchat/cdriveFeedbackList" component={CDriveFeedbackList} />
+        <Route path="/wchat/cdriveFeedbackList" component={CDriveFeedbackList} /> */}
 
         {/* PC后台 */}
         <Route path="/saicui/chat" component={Chat} alt='在线客服'/>
         <Route path="/saicui/materialList" component={MaterialList}  alt='图文素材管理'/>
-        <Route path="/saicui/news" component={News}  alt='资讯Demo'/>
+        {/* <Route path="/saicui/news" component={News}  alt='资讯Demo'/> */}
 
 
         <Route exact path="/" component={NotFoundPage} />
